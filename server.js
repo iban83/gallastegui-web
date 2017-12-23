0ce0cff3f96735f6de67bdbd4b13a64cf4f416ec
iban83 <34789198+iban83@users.noreply.github.com>
2017-12-23 11:11:32 +0100

    Update server.js
    Edit server.js.
    
    L116 server.js: 'self.app = express();'
    
    L25 server.js: self.ipaddress = '0.0.0.0';
    
    L26 server.js: self.port = 8080;
    
    Lines(L) are from the base V2 cartridge server.js.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var app = require('./app');
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = "0.0.0.0" ||process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.listen( port, ipaddress, function() {
